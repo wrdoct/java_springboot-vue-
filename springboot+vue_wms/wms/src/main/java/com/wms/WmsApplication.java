72e6bc529395cfc660ca14b5a7f7c21d0cebff1d
llw <llw18729357726@163.com>
2023-11-20 23:00:46 +0800

    仓库管理系统学习

---
+++ springboot+vue_wms/wms/src/main/java/com/wms/WmsApplication.java
@@ -0,0 +1,15 @@
+package com.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class WmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsApplication.class, args);
+    }
+
+}
