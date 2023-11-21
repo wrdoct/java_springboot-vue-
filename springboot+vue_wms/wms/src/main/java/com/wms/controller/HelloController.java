fb7e00236a901fee00ea1bf3e858208dde11c94f
llw <llw18729357726@163.com>
2023-11-21 22:15:59 +0800

    Mybatis配置及测试

--- springboot+vue_wms/wms/src/main/java/com/wms/controller/HelloController.java
+++ springboot+vue_wms/wms/src/main/java/com/wms/controller/HelloController.java
@@ -1,12 +1,26 @@
 package com.wms.controller;
 
+import com.wms.entity.User;
+import com.wms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
     @GetMapping
     public String hello(){
         return "hello wms";
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/list")
+    public List<User> list(){
+//        return userService.list();
+        return userService.listAll();
     }
 }
