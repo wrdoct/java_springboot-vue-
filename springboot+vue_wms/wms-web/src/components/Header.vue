947181275461182f22c2fdd5b88756bd6444b67f
llw <llw18729357726@163.com>
2023-11-28 22:09:07 +0800

    编写Header头页面

---
+++ springboot+vue_wms/wms-web/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div style="display: flex;line-height: 60px;">
+        <div style="margin-top: 8px;">
+            <i class="el-icon-s-fold" style="font-size: 20px"></i>
+        </div>
+        <div style="flex: 1;text-align: center;font-size: 34px;">
+            <span>欢迎来到仓库管理系统</span>
+        </div>
+        <el-dropdown>
+            <span>王小虎</span>
+            <i class="el-icon-arrow-down" style="margin-left: 5px;"></i>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item @click.native="toUser">个人中心</el-dropdown-item>
+                <el-dropdown-item @click.native="logOut">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        methods:{
+            toUser(){
+                console.log('to_user')
+            },
+            logOut(){
+                console.log('log_out')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
