8a5c70dadd66baa14f1f0d7b4498bae36a822161
llw <llw18729357726@163.com>
2023-12-01 21:31:19 +0800

    菜单导航页面伸缩功能代码编写

--- springboot+vue_wms/wms-web/src/components/Header.vue
+++ springboot+vue_wms/wms-web/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="display: flex;line-height: 60px;">
         <div style="margin-top: 8px;">
-            <i class="el-icon-s-fold" style="font-size: 20px"></i>
+            <i :class="icon" style="font-size: 20px;cursor: pointer;" @click="collapse"></i>
         </div>
         <div style="flex: 1;text-align: center;font-size: 34px;">
             <span>欢迎来到仓库管理系统</span>
@@ -20,12 +20,18 @@
 <script>
     export default {
         name: "Header",
+        props:{
+            icon:String
+        },
         methods:{
             toUser(){
                 console.log('to_user')
             },
             logOut(){
                 console.log('log_out')
+            },
+            collapse(){
+                this.$emit('doCollapse')
             }
         }
     }
--- springboot+vue_wms/wms-web/src/components/Index.vue
+++ springboot+vue_wms/wms-web/src/components/Index.vue
@@ -1,12 +1,12 @@
 <template>
     <el-container style="height: 100%; border: 1px solid #eee">
-        <el-aside width="200px" style="background-color: rgb(238, 241, 246); height: 100%">
-            <Aside></Aside>
+        <el-aside :width="aside_width" style="background-color: rgb(238, 241, 246); height: 100%; margin-left: -1px">
+            <Aside :isCollapse="isCollapse"></Aside>
         </el-aside>
 
         <el-container style="height: 100%">
             <el-header style="text-align: right; font-size: 12px; height: 100%;border-bottom: rgba(149,148,148,0.3) 1px solid;">
-                <Header></Header>
+                <Header @doCollapse="doCollapse" :icon="icon"></Header>
             </el-header>
 
             <el-main style = "height: 100%">
@@ -23,6 +23,25 @@
     export default {
         name: "Index",
         components: {Main, Header, Aside},
+        data(){
+            return {
+                isCollapse:false,
+                aside_width:'200px',
+                icon:'el-icon-s-fold'
+            }
+        },
+        methods:{
+            doCollapse(){
+                this.isCollapse = !this.isCollapse
+                if(!this.isCollapse){ //展开
+                    this.aside_width = '200px'
+                    this.icon = 'el-icon-s-fold'
+                }else { //收起
+                    this.aside_width = '64px'
+                    this.icon = 'el-icon-s-unfold'
+                }
+            }
+        }
     }
 </script>
 
