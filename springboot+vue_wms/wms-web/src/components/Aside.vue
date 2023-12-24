deacc73bc58e1df7fa5b9db0968c78ca704d34f4
llw <llw18729357726@163.com>
2023-12-24 15:38:53 +0800

    后退时左侧菜单也同时变化

--- springboot+vue_wms/wms-web/src/components/Aside.vue
+++ springboot+vue_wms/wms-web/src/components/Aside.vue
@@ -3,7 +3,7 @@
             background-color="#545c64"
             text-color="#fff"
             active-text-color="#ffd04b"
-            default-active="/home"
+            :default-active="$route.path"
             :collapse="isCollapse"
             :collapse-transition="false"
             router
